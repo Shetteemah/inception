1553738c0276c3640d7737ae9d7bb98893e34e57
Shetteemah <shettimaali58@yahoo.com>
2024-04-10 16:12:48 +0200

    Added user cridential in .env

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -3,7 +3,7 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=127.0.0.1 --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=$WP_NAME --dbuser=$WP_USER --dbpass=$WP_PASS --dbhost=$WP_HOST --allow-root
+./wp-cli.phar core install --url=$URL --title=$TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
 chown www-data:www-data /var/www/html
 php-fpm7.4 -F
