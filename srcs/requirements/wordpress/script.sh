458e30ce6b0d6320bebe152207cea55b949e8b07
Shetteemah <shettimaali58@yahoo.com>
2024-04-10 15:30:31 +0200

    Established connection between containers  added ``` networks:   inception: ```

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,6 +4,6 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-
+./wp-cli.phar core install --url=127.0.0.1 --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+chown www-data:www-data /var/www/html
 php-fpm7.4 -F
